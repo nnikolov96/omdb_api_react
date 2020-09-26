3ab0d791b79579f11a1fa5ea0174aa9a51b94279
Nikola <nikolanenchevnikolov@gmail.com>
2020-09-26 19:36:10 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,63 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import axios from 'axios';
+
+import Search from './components/Search';
+import Results from './components/Results';
+import PopUp from './components/PopUp';
 
 function App() {
+  const apiURL = 'http://www.omdbapi.com/?i=tt3896198&apikey=c36459f1';
+  const [state, setState] = useState({
+    s: '',
+    results: [],
+    selected: {}
+  });
+
+  const search = e => { 
+    if (e.key === 'Enter') { 
+      axios(apiURL + "&s=" + state.s).then(({data}) => { 
+        let results = data.Search;
+        setState(prevState => { 
+          return { ...prevState, results: results}
+        });
+      })
+    }
+  }
+
+
+  const handleInput = e => {
+    let s = e.target.value;
+    setState(prevState => { 
+      return { ...prevState, s: s}
+    });
+  }
+
+  const openPopup = id => { 
+    console.log('here');
+    axios(apiURL + "&id=" + id ).then(({data}) => { 
+      let result = data;
+      setState(prevState => { 
+        return { ...prevState, selected: result}
+      })
+    });
+  }
+
+  const closePopup = () => { 
+    setState(prevState => { 
+      return { ...prevState, selected: {}}
+    });
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div>
+      <header>
+        <h1>Movie Database</h1>
       </header>
+      <main>
+        <Search handleInput = {handleInput} search = { search } />
+        <Results results = {state.results} openPopup = {openPopup} />
+
+        {( typeof state.selected.Title != "undefined") ? <PopUp selected = {state.selected} closePopup = {closePopup}/> : false}
+      </main>
     </div>
   );
 }
